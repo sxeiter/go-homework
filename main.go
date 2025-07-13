4d3ecc3a400958e91c2c2dcd1fdf1033f0801f53
Pavel Baksheev <sxeiter@mail.ru>
2025-07-13 15:12:06 +0500

    hw2

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USD float64 = 1
 	const USDtoEUR float64 = USD * 1.1
 	const USDtoRUB float64 = USD * 80
+
+}
+
+func inputData() float64 {
+	var userInput float64
+	fmt.Println("Введите какое то значение")
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculate(money float64, usd float64, usdConvertTo float64) {
 
 }
